00f9d819c582cc019cef219189751ebde0522226
Matheus Muriel <matheus.muriel@outlook.com>
2020-05-31 14:02:48 -0300

    GeoChart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input'; 
-
 //
 
 import { MapsComponent } from './maps/maps.component';
